1121eca8281763cd28ec66380b0d22e8f002c24a
Kieran Duffy <Kieran.Duffy@students.ittralee.ie>
2020-11-18 16:47:00 +0000

    18/11/2020

--- taxcalculator/TestTaxCalculator.java
+++ taxcalculator/TestTaxCalculator.java
@@ -6,10 +6,10 @@ public class TestTaxCalculator {
     public static void main(String[] args) {
 
 
-        String name,status;
+        String name;
         int age;
-        char category;
-        double grossIncome, pension;
+        char category, status, dependant, children, blind;
+        double grossIncome1,grossIncome2, pension;
 
         Scanner input = new Scanner(System.in);
 
@@ -17,16 +17,33 @@ public class TestTaxCalculator {
         name = input.nextLine();
         System.out.print("Please enter your status:");
         name = input.nextLine();
-        System.out.print("Please enter your age:");
-        age = input.nextInt();
         System.out.print("Please enter your PRSI Category(F=Full-Rate, R=Reduced-Rate):");
         category = input.next().charAt(0);
         System.out.print("Please enter your Pension Contributions(If none, enter 0):");
         pension = input.nextDouble();*/
-        System.out.print("Please enter your Gross Income:");
-        grossIncome = input.nextDouble();
+        System.out.print("Please enter your Gross Income: ");
+        grossIncome1 = input.nextDouble();
+        System.out.print("Please enter your status(S=Single, M=Married): ");
+        status = input.next().toUpperCase().charAt(0);
+        System.out.print("If Married please enter your spouses income: ");
+        grossIncome2 = input.nextDouble();
+        System.out.print("Please enter your age:");
+        age = input.nextInt();
+        System.out.print("Do You have dependant children(Y=Yes, N=No): ");
+        children = input.next().toUpperCase().charAt(0);
+        System.out.print("Are you entitled to Blind person tax credit(Y=Yes, N=No): ");
+        blind = input.next().toUpperCase().charAt(0);
+        System.out.print("Are you entitled to Dependant Relative tax credit(Y=Yes, N=No): ");
+        dependant = input.next().toUpperCase().charAt(0);
 
-        System.out.print("\n\nTax Paid: " + String.format("%.2f",taxLiability(grossIncome)));
+
+        System.out.print("\n\nTax Paid: " + String.format("%.2f",taxLiability(grossIncome1, status, grossIncome2)));
+        System.out.print("\n\nTax Credits: " + String.format("%.2f",taxCredits(age, status, children, blind, dependant,
+                                                            grossIncome1, grossIncome2)));
+
+
+
+
 
 
 
@@ -74,16 +91,88 @@ public class TestTaxCalculator {
                      }
                      else
                          taxPaid = (married2Income*standardRate)+((income1+income2-married2Income)*higherRate);
-
-
-
-
-
-
-
+                     
         }
 
         return taxPaid;
+    }
+
+    public static double taxCredits(int age, char status, char children, char blind, char dependant, double income1, double income2){
+        final double ageSingleCredit=245;
+        final double ageMarriedCredit=490;
+        final double singleCredit=1650;
+        final double payeCredit=1650;
+        final double marriedCredit=3300;
+        final double singlePersonChildCarer=1650;
+        final double homeCarer=1600;
+        final double dependantCredit=70;
+        final double blindCredit=1650;
+        double totalTaxCredits = 0;
+
+        if(age >= 65){
+            if(status=='S'){
+                totalTaxCredits=ageSingleCredit+payeCredit+singleCredit;
+                if(children=='Y')
+                    totalTaxCredits+=singlePersonChildCarer;
+                if(blind=='Y')
+                    totalTaxCredits+=blindCredit;
+                if(dependant=='Y')
+                    totalTaxCredits+=dependantCredit;
+            }
+            else{
+                if(income2>0){
+                    totalTaxCredits=marriedCredit+marriedCredit+ageMarriedCredit;
+                    if(blind=='Y')
+                        totalTaxCredits+=blindCredit;
+                    if(dependant=='Y')
+                        totalTaxCredits+=dependantCredit;
+
+                }
+                else{
+                    totalTaxCredits=marriedCredit+payeCredit+ageMarriedCredit;
+                    if(children=='Y')
+                        totalTaxCredits+=homeCarer;
+                    if(blind=='Y')
+                        totalTaxCredits+=blindCredit;
+                    if(dependant=='Y')
+                        totalTaxCredits+=dependantCredit;
+                }
+            }
+
+        }
+        else{
+            if(status=='S'){
+                totalTaxCredits=payeCredit+singleCredit;
+                if(children=='Y')
+                    totalTaxCredits+=singlePersonChildCarer;
+                if(blind=='Y')
+                    totalTaxCredits+=blindCredit;
+                if(dependant=='Y')
+                    totalTaxCredits+=dependantCredit;
+            }
+            else{
+                if(income2>0){
+                    totalTaxCredits=marriedCredit+marriedCredit;
+                    if(blind=='Y')
+                        totalTaxCredits+=blindCredit;
+                    if(dependant=='Y')
+                        totalTaxCredits+=dependantCredit;
+
+                }
+                else{
+                    totalTaxCredits=marriedCredit+payeCredit;
+                    if(children=='Y')
+                        totalTaxCredits+=homeCarer;
+                    if(blind=='Y')
+                        totalTaxCredits+=blindCredit;
+                    if(dependant=='Y')
+                        totalTaxCredits+=dependantCredit;
+                }
+            }
+
+
+        }
+        return totalTaxCredits;
     }
 
 }
